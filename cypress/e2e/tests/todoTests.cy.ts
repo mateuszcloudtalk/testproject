eec8e3354c051fbf441ea9f7d8e9d4c80b326324
Mateusz Kozieł <mateusz.koziel@instapage.com>
2023-08-08 20:07:48 +0200

    semicolon fixes

--- cypress/e2e/tests/todoTests.cy.ts
+++ cypress/e2e/tests/todoTests.cy.ts
@@ -6,7 +6,7 @@ const TODO_ITEM_TWO = 'feed the cat';
 const todoPage = new TodoPage()
 describe('TODO list', () => {
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('/');
     })
     it('should add 2 elements to TODO list and verify its count', () => {
         todoPage.addTodoItem(TODO_ITEM_ONE);
@@ -16,7 +16,7 @@ describe('TODO list', () => {
         todoPage.getTodoListCount().should('eq', 2);
     })
     it('should toggle first item', () => {
-        cy.addTwoItems(TODO_ITEM_ONE, TODO_ITEM_TWO)
+        cy.addTwoItems(TODO_ITEM_ONE, TODO_ITEM_TWO);
         todoPage.toggleTodoItem(0);
         todoPage.getItem(0).should('have.class', 'completed');
     })
